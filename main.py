7d5755c7cd460352b7496206df59554127ec6bc7
Vedant Iyer <111111107+VI269@users.noreply.github.com>
2022-09-04 12:36:17 +0800

    Functions Added

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from random import randint,choice
+from os import system
+from time import sleep
 
+def wait(dur):
+  sleep(dur)
+  
+def clearConsole():
+  system('clear')
+  system('cls')
+  
+def loading(msg, dur):
+  for i in range(0, dur*3):
+    clearConsole()
+    print(f"{msg}.")
+    wait(1/3)
+    clearConsole()
+    print(f"{msg}..")
+    wait(1/3)
+    clearConsole()
+    print(f"{msg}...")
+    wait(1/3)
+    clearConsole()

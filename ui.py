2e1eae6cc7d0963cb2e0da6f1c6f5c0e8aa0e6b7
VI269 <111111107+VI269@users.noreply.github.com>
2022-09-07 16:33:27 +0800

    UI Finalized

--- ui.py
+++ ui.py
@@ -2,7 +2,13 @@ class UI:
   def __init__(self):
     pass
   def divider(self):
+    print('▂▃▅▇█▓▒░۩۞۩ ۩۞۩░▒▓█▇▅▃▂')
+  def divideret(self):
     return '▂▃▅▇█▓▒░۩۞۩ ۩۞۩░▒▓█▇▅▃▂'
+  def midt(self,text):
+    print(f'[{text}]')
+  def midtr(self,text):
+    return f'[{text}]'
 
 uitest = UI()
 

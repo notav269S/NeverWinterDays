258810ca7c6fdca3d170a8452b01842c58a5bf0b
VI269 <111111107+VI269@users.noreply.github.com>
2022-09-07 17:06:31 +0800

    Shop

--- main.py
+++ main.py
@@ -21,3 +21,30 @@ def loading(msg, dur):
     print(f"{msg}...")
     wait(1/3)
     clearConsole()
+
+class Shop:
+  def __init__(self):
+    self.stock = {}
+    self.prices = {}
+  def menu(self):
+    string = ''
+    for item in self.stock.keys():
+      for price in self.prices.keys():
+        string = string + (f'[{item[0].upper()+item[1:]}][Available:|{self.stock[item]}|Cost:|₪{self.prices[price]}|]\n\n')
+    return string
+  def stockUpdate(self,value):
+    for item in self.stock.key():
+      self.stock[item] += value
+  def newItem(self,name,avl,price):
+    self.stock[name] = avl
+    self.prices[name] = price
+  def delItem(self, name):
+    del self.stock[name]
+    del self.prices[name]
+
+newshop = Shop()
+newshop.newItem('apple',7,5)
+newshop.newItem('banana',7,5)
+newshop.newItem('orange',7,5)
+newshop.newItem('mango',7,5)
+print(newshop.menu())
